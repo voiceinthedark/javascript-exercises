72ef4d1d15f17a6eb6e6a692d80673f1df8b68ec
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-13 19:08:55 +0200

    Ex2: repeat string done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if(number >= 0){
+        let resultString = '';
+        for (let i = 0; i < number; i++) {
+            resultString += string;        
+        }
+        return resultString;
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
