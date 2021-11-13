bf7e8b5ebfc52281f956df11d51c41a42907c595
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-13 19:41:40 +0200

    Ex05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const range = function* (start, end) {
+    if(start > end){
+        [start, end] = [end, start];
+    }
 
+    while (start <= end) {
+        yield start++;
+    }
+}
+
+const sumAll = function(start, end) {
+    return Number.isInteger(start) 
+        && Number.isInteger(end) 
+        && start >= 0 && end >= 0
+        ? Array.from(range(start, end)).reduce((accumulator, current) =>        accumulator + current, 0)
+        : 'ERROR';
 };
+
+// let arr = Array.from(range(1,4))
+// console.log(arr);
 
 // Do not edit below this line
 module.exports = sumAll;
