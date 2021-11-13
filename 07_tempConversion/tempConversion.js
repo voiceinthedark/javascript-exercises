f71900870b0ba6fecb0a0799391ca3b3e5bd291c
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-13 20:17:33 +0200

    Ex07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(degree) {
+  return +((degree - 32) * (5/9)).toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(degree) {
+  return +(degree * (9 / 5) + 32).toFixed(1);
 };
 
 // Do not edit below this line
