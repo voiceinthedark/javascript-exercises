a1a3c7f4dde0992d23c4f1c104af3fdf649e6fb0
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-13 19:20:42 +0200

    Ex4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayValues, ...elements) {    
+    return arrayValues.filter(element => elements.includes(element) === false)
 };
 
 // Do not edit below this line
