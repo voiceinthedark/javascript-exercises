d9b2c3e869a2035bfe3b755af42cf5945a953498
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-16 17:18:47 +0200

    EX 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;  	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, cur) => acc * cur, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;	
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let range = function* (n) {
+    while(n > 1){
+      yield n--;
+    }
+  }
+  return multiply(Array.from(range(n)));
 };
 
 // Do not edit below this line
