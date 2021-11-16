2260ea060732a0996600004a45bb2d427957ee8a
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-16 17:33:46 +0200

    Ex 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    let filteredText = palindrome.match(/[A-Za-z]+/g);
+    let joined = filteredText.join("").toLowerCase();
+    let reversed = filteredText.join("").split('').reverse().join('').toLowerCase();
+    return joined === reversed;
 
+    // console.log(joined, reversed);
 };
 
+// palindromes("A car, a man, a maraca.");
 // Do not edit below this line
 module.exports = palindromes;
