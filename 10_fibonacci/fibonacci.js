ba9c17158fbaafc10bb7ba55398d14872b81ba8f
voiceinthedark <voiceinthedark@users.noreply.github.com>
2021-11-16 19:17:01 +0200

    EX 10 fibonaci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (+n >= 0) {
+    n = parseInt(n);
+    let a = (b = 1);
+    let fib = [a, b];
+    let serie = function* (n) {
+      do {
+        yield a + b;
+        [a, b] = [b, a + b];
+      } while (--n > 1);
+    };
+    return fib.concat(Array.from(serie(n)))[n - 1];
+  }
+  return "OOPS";
 };
+
+console.log(fibonacci(2));
 
 // Do not edit below this line
 module.exports = fibonacci;
